098e511ef5b9a16432e5f6a2d227547e44fa20d5
Jeff Van Nortwick <112516067+jeffvan98@users.noreply.github.com>
2024-06-09 13:13:28 -0400

    BRE experiments

---
+++ experiments/ExpToRules02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using RulesEngine;
+using RulesEngine.Models;
+
+JsonSerializerOptions options = new JsonSerializerOptions {
+    Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+};
+
+using var reader = File.OpenText("output.json");
+var rule = JsonSerializer.Deserialize<Rule>(reader.ReadToEnd(), options);
+
+var workflow = new Workflow();
+workflow.WorkflowName = "Test";
+workflow.Rules = new [] { rule };
+
+var engine = new RulesEngine.RulesEngine(new[] { workflow });
+
+//var test = new List<string> {"97126", "97350", "97838", "103086"};
+var test = new List<string> {"97126", "97350", "97838"};
+
+var results = await engine.ExecuteAllRulesAsync("Test", test);
+foreach (var result in results)
+{
+    Console.WriteLine($"Result: {result.Rule.RuleName} - {result.IsSuccess}");
+}
